509e94455cbbb4f41d4f7c47e53722144904b28e
jonathanBrian <jonathanbriangilabert@gmail.com>
2020-06-09 20:12:05 -0300

    0906: Commit inicial

---
+++ src/main/java/ar/edu/davinci/dvds20101cg10/DvDs20101cG10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20101cg10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20101cG10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20101cG10Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20101cg10/DvDs20101cG10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20101cg10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20101cG10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
