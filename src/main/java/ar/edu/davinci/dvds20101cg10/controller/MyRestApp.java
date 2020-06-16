d6ed94199a38391c94c7f6ecf46eabfa278f4348
jonathanBrian <jonathanbriangilabert@gmail.com>
2020-06-15 21:46:17 -0300

    1506: Se agrega package de Controller

---
+++ src/main/java/ar/edu/davinci/dvds20101cg10/controller/MyRestApp.java
@@ -0,0 +1,10 @@
+package ar.edu.davinci.dvds20101cg10.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/myrestapp")
+public abstract class MyRestApp {
+
+}
