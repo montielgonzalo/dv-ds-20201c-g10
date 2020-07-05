ec117f33a493b38c45a662e3d712ed051e460394
jonathanBrian <jonathanbriangilabert@gmail.com>
2020-07-05 16:37:17 -0300

    0507: Se agrega script de creacion de base de datos

---
+++ .sql/scriptDeCreacion.sql
@@ -0,0 +1,35 @@
+-- EJECUTAR PARA CREAR LA TABLA
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `cli_id` int NOT NULL AUTO_INCREMENT,
+  `cli_nombre` varchar(50) DEFAULT NULL,
+  `cli_apellido` varchar(50) DEFAULT NULL,
+  `cli_email` varchar(50) DEFAULT NULL,
+  `cli_password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`cli_id`),
+  UNIQUE KEY `cli_email_UNIQUE` (`cli_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Juan','Perez','juan@p.com','1234');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-08  3:47:55
